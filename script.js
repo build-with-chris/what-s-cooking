d48701ec168381a6853f999731eace434036d3f7
hecking_killa <chris.hermann9397@gmail.com>
2025-06-01 18:22:18 +0200

    modularizing the script.js

--- script.js
+++ script.js
@@ -2,28 +2,48 @@ const recipeForm = document.querySelector('.recipe-form');
 const recipeCollection = document.querySelector('.recipe-collection');
 let recipes = [];
 
-if (localStorage.getItem('recipes')) {
-    recipes = JSON.parse(localStorage.getItem('recipes'));
+
+function initRecipes() {
+    const json = localStorage.getItem('recipes')
+    if (!json) return []
+    try {
+        return JSON.parse(json)
+    } catch (e) {
+        console.log('Could not parse the json', e)
+        return []
+    }
+    renderRecipes
+}
+
+/** add a new recipe object and save it in localStorage + render of the new items
+ * @param {{ title: string, ingredients: string, steps: string, recipeImage: string }} recipeObj
+ */
+
+function addRecipe(recipeObj) {
+    recipes.push(recipeObj);
+    localStorage.setItem('recipes', JSON.stringify(recipes));
+
     renderRecipes();
 }
 
+
 recipeForm.addEventListener('submit', function(event) {
     event.preventDefault();
-    const title = document.getElementById('title').value.trim();
-    const ingredients = document.getElementById('ingredients').value.trim();
-    const steps = document.getElementById('steps').value.trim();
-    const recipeImage = document.getElementById('recipeImage').value.trim();
+    const formData = new FormData(recipeForm);
+    const title = formData.get("title").trim();
+    const ingredients = formData.get('ingredients').trim();
+    const steps = formData.get('steps').trim();
+    const recipeImage = formData.get('recipeImage').trim();
 
     if (!title || !ingredients || !steps || !recipeImage) {
         alert('Fill out all fields');
         return;
     }
-    recipes.push({ title, ingredients, steps, recipeImage });
-    localStorage.setItem('recipes', JSON.stringify(recipes));
-
-    renderRecipes();
+    
+    addRecipe({ title, ingredients, steps, recipeImage })
     recipeForm.reset();
 });
+
 
 function renderRecipes() {
     recipeCollection.innerHTML = recipes.map( (r, i) => `
@@ -40,13 +60,24 @@ function renderRecipes() {
     deleteRecipe();
 }
 
+
 function deleteRecipe() {
     document.querySelectorAll('.delete').forEach(btn => {
         btn.addEventListener('click', () => {
+            const confirmed = confirm("Do you really want to delete this recipe?")
+            if (!confirmed) {
+                return;
+            }
             const idx = Number(btn.dataset.index);
             recipes.splice(idx, 1);
+            localStorage.setItem('recipes', JSON.stringify(recipes));
             renderRecipes();
         })
     })
 
 }
+
+window.addEventListener('DOMContentLoaded', () => {
+    recipes = initRecipes();
+    renderRecipes();
+  });
